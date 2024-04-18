4fa73dd4bcac2a1576aa11972b4590633f0d6b7c
Niki Powell <nhe.powell@gmail.com>
2024-04-18 16:04:15 -0500

    started staff homepage and staff navbar

--- src/components/Employee/EmployeeHome.jsx
+++ src/components/Employee/EmployeeHome.jsx
@@ -1,11 +1,19 @@
 import { useEffect, useState } from "react"
-// import { FilterBar } from "./FilterBar.jsx"
 import { getAllRoutes } from "../../services/routeService.js"
+import { RouteObject } from "../route/RouteObject.jsx"
+import { FilterBar } from "./FilterBar.jsx"
 
 
 export const EmployeeHome = () => {
 
     const [allRoutes, setAllRoutes] = useState([])
+    const [selectedType, setSelectedType] = useState('')
+    const [selectedGrade, setSelectedGrade] = useState('')
+    const [selectedSetter, setSelectedSetter] = useState('')
+    const [filteredRouteTypes, setFilteredRouteTypes] = useState([])
+    const [filteredRouteGrades, setFilteredRouteGrades] = useState([])
+    const [filteredRouteSetters, setFilteredRouteSetters] = useState([])
+    const [displayedRoutes, setDisplayedRoutes] = useState([])
 
     useEffect(() => {
         getAllRoutes().then(routesArray => {
@@ -13,16 +21,49 @@ export const EmployeeHome = () => {
         }) 
     }, [])
 
+    useEffect(() => {
+        if (parseInt(selectedType) > 0) {
+            const filteredTypes = allRoutes.filter(route => route.typeId === parseInt(selectedType))
+            setDisplayedRoutes(filteredTypes)
+        } else {
+            setDisplayedRoutes(allRoutes)
+        }
+    }, [allRoutes, selectedType])
+
+    useEffect(() => {
+        if (parseInt(selectedGrade) > 0) {
+            const filteredGrades = allRoutes.filter(route => route.gradeId === parseInt(selectedGrade))
+            setDisplayedRoutes(filteredGrades)
+        } else {
+            setDisplayedRoutes(allRoutes)
+        }
+    }, [allRoutes, selectedGrade])
+
+    useEffect(() => {
+        if (parseInt(selectedSetter) > 0) {
+            const filteredSetters = allRoutes.filter(route => route.userId === parseInt(selectedSetter))
+            setDisplayedRoutes(filteredSetters)
+        } else {
+            setDisplayedRoutes(allRoutes)
+        }
+    }, [allRoutes, selectedSetter])
 
 
     return (
         <div className="employee-home">
+            <h2>Gym Routes</h2>
             <div className="employee-route-filter">
-                {/* <FilterBar /> */}
+                <FilterBar 
+                    allRoutes={allRoutes} 
+                    setSelectedType={setSelectedType}
+                    setSelectedGrade={setSelectedGrade} 
+                    setSelectedSetter={setSelectedSetter} 
+                />
             </div>
-            <div className="routes-section">
-                <h2>Gym Routes</h2>
-
+            <div className="routes">
+                {displayedRoutes.map(route => {
+                    return <RouteObject route={route} key={route.id} />
+                })}
             </div>
         </div>
     )
--- src/components/Employee/FilterBar.jsx
+++ src/components/Employee/FilterBar.jsx
@@ -1,10 +1,57 @@
+import { useEffect, useState } from "react"
+import { getRouteGrades, getRouteSetters, getRouteTypes } from "../../services/routeService.js"
 
-export const FilterBar = () => {
+
+
+export const FilterBar = ({allRoutes, setSelectedSetter, setSelectedGrade, setSelectedType, setDisplayedRoutes}) => {
+    const [types, setTypes] = useState([])
+    const [grades, setGrades] = useState([])
+    const [setters, setSetters] = useState([])
+
+
+    // get all route types
+    useEffect(() => {
+         getRouteTypes().then(types => {
+            setTypes(types)
+         })
+    },[allRoutes])
+
+    // get all route grades
+    useEffect(() => {
+        getRouteGrades().then(grades => {
+            setGrades(grades)
+        }) 
+    },[allRoutes])
+
+    // get all route setters
+    useEffect(() => {
+        getRouteSetters().then(setters => {
+            setSetters(setters)
+        }) 
+    }, [allRoutes])
+
 
 
     return (
         <div className="filter-bar">
-            
+            <select onChange={(event) => {setSelectedType(event.target.value)}} id="type-select" className="filter-item">
+                <option value="0" id="type">select route type...</option>
+                    {types.map(type => {
+                        return <option value={type.id} key={type.id}>{type.name}</option>
+                    })}
+            </select>
+            <select onChange={(event) => {setSelectedGrade(event.target.value)}} id="grade-select" className="filter-item">
+                <option value="0" id="grade">select grade...</option>
+                    {grades.map(grade => {
+                        return <option value={grade.id} key={grade.id}>{grade.name}</option>
+                    })}
+            </select>
+            <select onChange={(event) => {setSelectedSetter(event.target.value)}} id="setter-select" className="filter-item">
+                <option value="0" id="setter">select setter...</option>
+                    {setters.map(setter => {
+                        return <option value={setter.id} key={setter.id}>{setter.name}</option>
+                    })}
+            </select>
         </div>
     )
 }
