b7d462534bfd0d6614831ee427ce00963486e3b5
Niki Powell <nhe.powell@gmail.com>
2024-04-24 18:01:44 -0500

    played around with styling

--- src/components/navigation/MemberNav.jsx
+++ src/components/navigation/MemberNav.jsx
@@ -6,31 +6,39 @@ export const MemberNav = () => {
 
     return (
         <ul className="navbar">
-            <li className="navbar-info">
-                <Link to="home">My Routes</Link>
-            </li>
-            <li className="navbar-info">
-                <Link to="myprofile">My Profile</Link>
-            </li>
-            <li className="navbar-info">
-                <Link to="search">Route Finder</Link>
-            </li>
-
-            {localStorage.getItem("sendit_user") ? (
-                <li className="navbar-info navbar-logout">
-                    <Link
-                    to=""
-                    onClick={() => {
-                        localStorage.removeItem("sendit_user")
-                        navigate("/login", { replace: true })
-                    }}
-                    >
-                    Logout
-                    </Link>
+            <div className="navbar-title">
+                <li className="navbar-info">
+                    <Link className="nav-text">send<span className="green-font">it</span></Link>
                 </li>
-            ) : (
-                ""
-            )}
+            </div>
+            <div className="navbar-routes">
+                <li className="navbar-info">
+                    <Link className="nav-text" to="home">my routes</Link>
+                </li>
+                <li className="navbar-info">
+                    <Link className="nav-text" to="myprofile">profile</Link>
+                </li>
+                <li className="navbar-info">
+                    <Link className="nav-text" to="search">route finder</Link>
+                </li>
+            </div>
+            <div className="navbar-logout">
+                {localStorage.getItem("sendit_user") ? (
+                    <li className="navbar-info navbar-logout">
+                        <Link
+                        to=""
+                        onClick={() => {
+                            localStorage.removeItem("sendit_user")
+                            navigate("/login", { replace: true })
+                        }}
+                        >
+                        logout
+                        </Link>
+                    </li>
+                ) : (
+                    ""
+                )}
+            </div>
         </ul>
     )
 }
