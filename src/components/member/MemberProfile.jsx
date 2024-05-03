dedb08f260addd597db882ef961b8c8d8daa6970
Niki Powell <nhe.powell@gmail.com>
2024-05-03 12:08:52 -0500

    styling

--- src/components/member/MemberProfile.jsx
+++ src/components/member/MemberProfile.jsx
@@ -4,28 +4,42 @@ import './memberProfile.css'
 
 
 export const MemberProfile = ({currentUser}) => {
-    const [member, setMember] = useState({})
+    const [memberExpandTicks, setMemberExpandTicks] = useState({})
+    const [displayedTicks, setDisplayedTicks] = useState([])
 
     useEffect(() => {
         getMembersById(currentUser.id).then(memberData => {
             const memberObject = memberData[0]
-            setMember(memberObject)
+            setMemberExpandTicks(memberObject)
         }) 
     },[])
 
+    // useEffect(() => {
+    //     const favoriteRoutes = memberExpandTicks.filter(member => {
+    //         return member.ticks.some(tick => tick.rating > 3)
+    //     })
+    //     setDisplayedTicks(favoriteRoutes)
+    // },[currentUser, memberExpandTicks])
+
     return (
-        <div className="member-profile">
-            <div className="member-profile-container">
-                <div className="member-profile-picture-containter">
-                    <div className="member-profile-picture">
-                        <img />
+        <div className="member-profile-container">
+
+            <div className="circular-gradient">
+
+                <div className="member-profile-details">
+
+                    <div className="member-profile-information">
+                        <div className="member-name">{memberExpandTicks?.fullName}</div>
+                        <div className="member-sends">Total sends: {memberExpandTicks.ticks?.length}</div>
+                    </div>
+                    <div className="member-profile-tick-container">
+                        {/* <h3 className="heading">favorite climbs</h3> */}
+                            
                     </div>
                 </div>
-                <div className="member-profile-information">
-                    <div className="member-name">{member?.fullName}</div>
-                    <div className="member-sends">Total sends: {member.ticks?.length}</div>
-                </div>
+
             </div>
+
         </div>
     )
 }
